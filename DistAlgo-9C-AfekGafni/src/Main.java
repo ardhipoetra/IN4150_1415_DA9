d7fcc583e8cab69fc29fcbaf68717594c140c027
ardhipoetra <ardhipoetra@yahoo.com>
2015-05-13 14:52:16 +0200

    start simultaneously

--- DistAlgo-9C-AfekGafni/src/Main.java
+++ DistAlgo-9C-AfekGafni/src/Main.java
@@ -1,15 +1,17 @@
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
-import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.concurrent.CountDownLatch;
 
 
 public class Main {
 
-	public final static int NODES = 5;
+	public final static int NODES = 50;
 	
 	public final static String TAG = "[MAIN]";
+	
+	private final static CountDownLatch countDownLatch  = new CountDownLatch(1);
 	
 	public static void main(String[] args) throws RemoteException, MalformedURLException, InterruptedException, NotBoundException, AlreadyBoundException {
 		OrdinaryProcess opinit;
@@ -20,9 +22,47 @@ public class Main {
 //			Naming.rebind("rmi://localhost/AfganOP"+i, opinit);
 		}
 		
-		new CandidateProcess(2);
-		new CandidateProcess(0);
+		Thread t1 = new Thread() {
+			@Override
+			public void run() {
+				try {
+					countDownLatch.await();
+					new CandidateProcess(21);
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
 		
+		Thread t2 = new Thread() {
+			@Override
+			public void run() {
+				try {
+					countDownLatch.await();
+					new CandidateProcess(0);
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		t1.start();
+		t2.start();
+		countDownLatch.countDown();
 		
 		
 	}
